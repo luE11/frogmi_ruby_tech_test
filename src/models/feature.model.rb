f51d764f4e4159318193a7ec58cd2f81299a8a25
Luis Martínez <luisedu555@gmail.com>
2024-04-06 10:03:16 -0500

    Feature sequel model

---
+++ src/models/feature.model.rb
@@ -0,0 +1,12 @@
+class Feature < Sequel::Model
+  def validate
+    super
+    errors.add(:title, "can't be empty") if title.empty?
+    errors.add(:url, "can't be empty") if url.empty?
+    errors.add(:place, "can't be empty") if place.empty?
+    errors.add(:magType, "can't be empty") if magType.empty?
+    errors.add(:mag, "must be between -1.0 and 10.0") if mag<-1 || mag>10
+    errors.add(:latitude, "must be between -90.0 and 90.0") if latitude<-90 || latitude>90
+    errors.add(:longitude, "must be between -180.0 and 180.0") if longitude<-180 || longitude>180
+  end
+end
