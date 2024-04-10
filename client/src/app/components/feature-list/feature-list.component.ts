f1865a77cad7a8cdb59077bb4884d60ed2089579
Luis Martínez <luisedu555@gmail.com>
2024-04-09 22:03:36 -0500

    features table without stylees and filter options

--- client/src/app/components/feature-list/feature-list.component.ts
+++ client/src/app/components/feature-list/feature-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FeatureFilter } from 'src/app/interfaces/feature-filter';
+import { FeatureList } from 'src/app/interfaces/feature-list';
+import { FeatureService } from 'src/app/services/feature.service';
 
 @Component({
   selector: 'app-feature-list',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./feature-list.component.css']
 })
 export class FeatureListComponent {
+
+  features?: FeatureList;
+  private filter: FeatureFilter = {}
+
+  constructor(private featureService: FeatureService) { }
+
+  ngOnInit(){
+    this.getFeatures()
+  }
+
+  getFeatures(){
+    this.featureService.getFeatures(this.filter)
+      .subscribe(data => {
+        this.features = data
+      })
+  }
 
 }
